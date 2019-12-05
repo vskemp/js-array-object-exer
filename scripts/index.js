d8eef7a99a9675cd1a775a6895e0fee9eb86f123
Veronica Kemp <veronicakemp@Veronicas-MacBook-Pro.local>
2019-12-04 19:01:54 -0500

    exerciese complete. Boolean issues.

--- scripts/index.js
+++ scripts/index.js
@@ -3,6 +3,9 @@
 // (If you use loops, make sure they are part of the function.)
 ////////////////////////////////////////
 
+// WITH POSITIVE AND EVEN NUM EXERCISES: convert boolean to intagers????
+
+
 // Positive Numbers
 // Write a function which takes an array of numbers as input and returns a new array 
 // containing only the positive numbers in the given array.
@@ -27,13 +30,13 @@
 // }
 // console.log(posNums(numArray));
 
-////////////////////////////////////////
+// ////////////////////////////////////////
 
-// Even Numbers
-// Write a function which takes an array of numbers as input and returns a new array containing 
-// only the even numbers in the given array.
+// // Even Numbers
+// // Write a function which takes an array of numbers as input and returns a new array containing 
+// // only the even numbers in the given array.
 
-// const numArray = [
+// const newArray = [
 //     7,
 //     18,
 //     56,
@@ -44,90 +47,205 @@
 //     5,
 // ];
 
-// function evenNums(numArray) {
+// function evenNums(newArray) {
 //     let evenArray = [];
-//     for  (let nums of numArray) {
+//     for  (let nums of newArray) {
 //         nums = nums % 2 == 0;
 //         evenArray.push(nums);
 //     }
 //     return evenArray;
 // }
-// console.log(evenNums(numArray));
+// console.log(evenNums(newArray));
 
-////////////////////////////////////////
+// ////////////////////////////////////////
 
-// Square the Numbers
-// Write a function which takes an array of numbers as its argument and returns a new array 
-// containing result of squaring each of the numbers in the given array by two. Example: 
-// squareTheNumbers([1, 2, 3]) should give [1, 4, 9].
+// // Square the Numbers
+// // Write a function which takes an array of numbers as its argument and returns a new array 
+// // containing result of squaring each of the numbers in the given array by two. Example: 
+// // squareTheNumbers([1, 2, 3]) should give [1, 4, 9].
 
-// const array = [
-//     1,
-//     6,
-//     5,
-//     10,
-//     11,
-//     2
-// ]
+// // const array = [
+// //     1,
+// //     6,
+// //     5,
+// //     10,
+// //     11,
+// //     2
+// // ]
 
-// function squareNum(array) {
-//     let newArray = [];
-//     for (let square of array){
-//         square = Math.pow(square, 2);
-//         newArray.push(square);
-//     }
-//     return newArray;
-// }
-// console.log(squareNum(array));
+// // function squareNum(array) {
+// //     let newArray = [];
+// //     for (let square of array){
+// //         square = Math.pow(square, 2);
+// //         newArray.push(square);
+// //     }
+// //     return newArray;
+// // }
+// // console.log(squareNum(array));
 
-////////////////////////////////////////
+// ////////////////////////////////////////
 
-// Cities 1
-// Write a function which takes an array of city objects like such:
+// // Cities 1
+// // Write a function which takes an array of city objects like such:
 
-// const cities = [
-//     { name: 'Los Angeles', temperature: 60.0},
-//     { name: 'Atlanta', temperature: 52.0 },
-//     { name: 'Detroit', temperature: 48.0 },
-//     { name: 'New York', temperature: 80.0 }
-// ];
-// // // as input and returns a new array containing the cities whose temperature is cooler than
-// // // 70 degrees.
+// // const cities = [
+// //     { name: 'Los Angeles', temperature: 60.0},
+// //     { name: 'Atlanta', temperature: 52.0 },
+// //     { name: 'Detroit', temperature: 48.0 },
+// //     { name: 'New York', temperature: 80.0 }
+// // ];
+// // // // as input and returns a new array containing the cities whose temperature is cooler than
+// // // // 70 degrees.
 
-// function belowSeventy(array) {
-//     let newArray = [];
-//     for (let city of array) {
-//         console.log(city['temperature']);
-//         if (city.temperature < 70) {
-//             newArray.push(city.name);
-//         }
-//     }
-//     return newArray;
-// }
+// // function belowSeventy(array) {
+// //     let newArray = [];
+// //     for (let city of array) {
+// //         console.log(city['temperature']);
+// //         if (city.temperature < 70) {
+// //             newArray.push(city.name);
+// //         }
+// //     }
+// //     return newArray;
+// // }
 
-// console.log(belowSeventy(cities));
+// // console.log(belowSeventy(cities));
 
-////////////////////////////////////////
+// ////////////////////////////////////////
 
-// Cities 2
-// Write a function which takes an array of city objects like the above problem as input and 
-// returns an array of the cities names.
+// // Cities 2
+// // Write a function which takes an array of city objects like the above problem as input and 
+// // returns an array of the cities names.
 
-// function cityNames(array) {
-//     let newArray = [];
-//     for (let city of array) {
-//         console.log(city["temperature"]);
-//     newArray.push(city.name);
-//     }
-//     return newArray;
-// }
+// // function cityNames(array) {
+// //     let newArray = [];
+// //     for (let city of array) {
+// //         console.log(city["temperature"]);
+// //     newArray.push(city.name);
+// //     }
+// //     return newArray;
+// // }
 
-// console.log(cityNames(cities));
+// // console.log(cityNames(cities));
 
-////////////////////////////////////////
+// ////////////////////////////////////////
 
-// Good Job!
-// Given an array of people's names:
+// // Good Job!
+// // Given an array of people's names:
+
+// // const people = [
+// //     'Dom',
+// //     'Lyn',
+// //     'Kirk',
+// //     'Autumn',
+// //     'Trista',
+// //     'Jesslyn',
+// //     'Kevin',
+// //     'John',
+// //     'Eli',
+// //     'Juan',
+// //     'Robert',
+// //     'Keyur',
+// //     'Jason',
+// //     'Che',
+// //     'Ben'
+// // ];
+// // // Print out 'Good Job, {{name}}!' for each person's name, one on a line.
+// // people.forEach(function(people) {
+// //     console.log(`Good Job ${people}!`);
+// // })
+
+// ////////////////////////////////////////
+
+// // 3 times
+// // Given this function:
+
+// // function call3Times(fun) {
+// //   fun();
+// //   fun();
+// //   fun();
+// // }
+// // Use the call3Times function to print "Hello, world!" 3 times.
+
+// // function fun() {
+// //     console.log("Hello, world!");
+// // }
+
+// // function call3Times(fun) {
+// //     fun();
+// //     fun();
+// //     fun();
+// // }
+
+// // call3Times(fun);
+
+
+// ////////////////////////////////////////
+
+// // n times
+// // You will write a function callNTimes that takes two arguments: times as a number, and fun as
+// // a function. It will call that function for that many times. Example:
+
+// // > callNTimes(5, hello)
+// // Hello, world!
+// // Hello, world!
+// // Hello, world!
+// // Hello, world!
+// // Hello, world!
+// // You are allowed to use a loop in the implementation of callNTimes.
+
+
+// // function callNTimes(times, fun) {
+// //     for (let i = 0; i < times; i++) {
+// //         fun();
+// //     }
+// // }
+
+// // callNTimes(5, fun);
+
+// ////////////////////////////////////////
+
+// // Str Multiply
+// // Write a strMultiply function which takes two arguments:
+
+// // str - the string to multiply
+// // times - number of times to multiply
+// // Example:
+
+// // > strMultiply('abc', 5)
+// // 'abcabcabcabcabc'
+// // You may not use the native repeat method that strings provide. But you can use the range 
+// // function defined below.
+
+// // You may use the following range function as is:
+
+// // function range(min, max) {
+// //     const arr = [];
+// //     for (var i = min; i < max; i++) {
+// //     arr.push(i);
+// // }
+// //     return arr;
+// // }
+
+// // function strMultiply(str, times) {
+// //     let newStr = '';
+// //     for (let i = 0; i < times; i++) {
+// //         newStr += str;
+// //     }
+// //     return newStr;
+// // }
+
+// // console.log(strMultiply('bnbnbn', 10));
+
+// ////////////////////////////////////////
+
+// // Bonus: Array sorting
+// // For these exercises, you'll want to refer to the MDN docs on using Array's sort method.
+
+// ////////////////////////////////////////
+
+// // Sort an array
+// // Given an array of strings such as the array of names given in one of the "Good Job" problem, 
+// // sort them by alphabetically order.
 
 // const people = [
 //     'Dom',
@@ -146,99 +264,25 @@
 //     'Che',
 //     'Ben'
 // ];
-// // Print out 'Good Job, {{name}}!' for each person's name, one on a line.
-// people.forEach(function(people) {
-//     console.log(`Good Job ${people}!`);
-// })
 
-////////////////////////////////////////
+// // console.log(people.sort());
 
-// 3 times
-// Given this function:
+// // ////////////////////////////////////////
 
-// function call3Times(fun) {
-//   fun();
-//   fun();
-//   fun();
-// }
-// Use the call3Times function to print "Hello, world!" 3 times.
+// // // Sort an array, 2
+// // // Sort the same array, but not by alphabetically order, but by how long each name is, 
+// // // shortest name first.
 
-// function fun() {
-//     console.log("Hello, world!");
-// }
-
-// function call3Times(fun) {
-//     fun();
-//     fun();
-//     fun();
-// }
-
-// call3Times(fun);
-
-
-////////////////////////////////////////
-
-// n times
-// You will write a function callNTimes that takes two arguments: times as a number, and fun as
-// a function. It will call that function for that many times. Example:
-
-// > callNTimes(5, hello)
-// Hello, world!
-// Hello, world!
-// Hello, world!
-// Hello, world!
-// Hello, world!
-// You are allowed to use a loop in the implementation of callNTimes.
-
-
-// function callNTimes(times, fun) {
-//     for (let i = 0; i < times; i++) {
-//         fun();
+// function nameLength(a, b) {
+//     if (a.length < b.length) {
+//         return -1;
 //     }
+//     if (a.length > b.length) {
+//         return 1;
+//     }
+//     return 0;
 // }
-
-// callNTimes(5, fun);
-
-////////////////////////////////////////
-
-// Str Multiply
-// Write a strMultiply function which takes two arguments:
-
-// str - the string to multiply
-// times - number of times to multiply
-// Example:
-
-// > strMultiply('abc', 5)
-// 'abcabcabcabcabc'
-// You may not use the native repeat method that strings provide. But you can use the range 
-// function defined below.
-
-// You may use the following range function as is:
-
-function range(min, max) {
-    const arr = [];
-    for (var i = min; i < max; i++) {
-    arr.push(i);
-}
-    return arr;
-}
-
-////////////////////////////////////////
-
-// Bonus: Array sorting
-// For these exercises, you'll want to refer to the MDN docs on using Array's sort method.
-
-////////////////////////////////////////
-
-// Sort an array
-// Given an array of strings such as the array of names given in one of the "Good Job" problem, 
-// sort them by alphabetically order.
-
-////////////////////////////////////////
-
-// Sort an array, 2
-// Sort the same array, but not by alphabetically order, but by how long each name is, 
-// shortest name first.
+// console.log(people.sort(nameLength));
 
 ////////////////////////////////////////
 
@@ -246,9 +290,21 @@ function range(min, max) {
 // Given an array of array of products:
 
 // const products = [
-//   { name: 'Basketball', price: 12.00 },
-//   { name: 'Tennis Racquet', price: 66.00 },
-//   { name: 'Tennis Balls', price: 9.00 },
-//   { name: 'Tennis Balls', price: 9.00 }
+//     { name: 'Basketball', price: 12.00 },
+//     { name: 'Tennis Racquet', price: 66.00 },
+//     { name: 'Tennis Balls', price: 9.00 },
+//     { name: 'Tennis Balls', price: 9.00 }
 // ];
 // Sort the array by price.
+
+// function proPrice(a, b) {
+//     if (a.price < b.price) {
+//         return 1;
+//     }
+//     if (a.price > b.price) {
+//         return -1;
+//     }
+//     return 0;
+// }
+
+// console.log(products.sort(proPrice));
